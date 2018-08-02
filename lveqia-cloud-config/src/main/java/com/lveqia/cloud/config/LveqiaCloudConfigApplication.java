ef165a46e82a4be2c374f35b2ceb32bdb81a2a74
leolaure <leolaurel.e.l@gmail.com>
2018-08-02 14:53:38 +0800

    删除无用模块

--- lveqia-cloud-config/src/main/java/com/lveqia/cloud/config/LveqiaCloudConfigApplication.java
+++ lveqia-cloud-config/src/main/java/com/lveqia/cloud/config/LveqiaCloudConfigApplication.java
@@ -19,8 +19,4 @@ public class LveqiaCloudConfigApplication {
         SpringApplication.run(LveqiaCloudConfigApplication.class, args);
     }
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World!";
-    }
 }
