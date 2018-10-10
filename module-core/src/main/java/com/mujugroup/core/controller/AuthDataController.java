bbf0c5ea59241b5287b31d788b4efe3c1ed7ee07
longpeng <longpeng@mujugroup.com>
2018-10-10 16:39:47 +0800

    添加获取选定用户的数据权限的方法

--- module-core/src/main/java/com/mujugroup/core/controller/AuthDataController.java
+++ module-core/src/main/java/com/mujugroup/core/controller/AuthDataController.java
@@ -1,8 +1,10 @@
 package com.mujugroup.core.controller;
 
 
+import com.lveqia.cloud.common.objeck.DBMap;
 import com.lveqia.cloud.common.util.ResultUtil;
 import com.mujugroup.core.objeck.bo.TreeBO;
+import com.mujugroup.core.objeck.vo.TreeVO;
 import com.mujugroup.core.service.AuthDataService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -34,25 +38,14 @@ public class AuthDataController {
 
     @ApiOperation(value = "查询查询树结构（代理商、医院、科室）", notes = "组合多表，生成树形结构数据")
     @RequestMapping(value = "/tree", method = RequestMethod.GET)
-    public String tree(@ApiParam(hidden = true) int uid) {
-        List<TreeBO> aidList = authDataService.getAgentAuthData(uid);
-        List<TreeBO> hidList = authDataService.getHospitalAuthData(uid);
-        if (aidList.size() == 0 && hidList.size() == 0) {
-            List<TreeBO> allList = authDataService.getAllAgentList();
-            return ResultUtil.success(authDataService.treeBoToVo(allList));
-        }
-        if (hidList.size() > 0) {
-            TreeBO treeBO = new TreeBO();
-            treeBO.setId("AID0");
-            treeBO.setName("其他可选医院");
-            treeBO.setDisabled(true);
-            treeBO.setChildren(authDataService.toJsonString(hidList));
-            aidList.add(treeBO);
-        }
-        return ResultUtil.success(authDataService.treeBoToVo(aidList));
+    public String tree(@ApiParam(hidden = true) int uid, @ApiParam(value = "userId") @RequestParam(value = "userId"
 
+            , required = false, defaultValue = "0") int userId) {
+        String result = userId == 0 ? ResultUtil.success(getTreeBOList(uid)) : ResultUtil.success(getTreeBOList(userId));
+        return result;
     }
-    @ApiOperation(value = "更新数据权限",notes = "更新数据权限")
+
+    @ApiOperation(value = "更新数据权限", notes = "更新数据权限")
     @RequestMapping(value = "/modify", method = RequestMethod.POST)
     public String updateAuthData(@ApiParam(value = "数据权限") @RequestParam(value = "authData"
             , required = false) String[] authData, @RequestParam("uid") int uid) {
@@ -63,4 +56,45 @@ public class AuthDataController {
             return ResultUtil.error(ResultUtil.CODE_UNKNOWN_ERROR, "修改权限失败!");
         }
     }
+
+    @ApiOperation(value = "获取选定用户的数据权限", notes = "获取选定用户的数据权限")
+    @RequestMapping(value = "/select", method = RequestMethod.GET)
+    public String getAuthDataById(@ApiParam(value = "userId") @RequestParam(value = "userId") int userId) {
+        List<DBMap> authDataList = authDataService.getAuthData(userId);
+        List<String> list = new ArrayList<String>();
+        if (authDataList != null && authDataList.size() > 0) {
+            for (DBMap dbMap : authDataList) {
+                if (Integer.parseInt(dbMap.getKey()) == 1) {
+                    list.add("AID" + dbMap.getValue());
+                } else if (Integer.parseInt(dbMap.getKey()) == 2) {
+                    list.add("HID" + dbMap.getValue());
+                } else {
+                    list.add("OID" + dbMap.getValue());
+                }
+            }
+            return ResultUtil.success(list);
+        } else {
+            return ResultUtil.error(ResultUtil.CODE_NOT_FIND_DATA);
+        }
+    }
+
+    //数据权限树
+    private List<TreeVO> getTreeBOList(int id) {
+        List<TreeBO> aidList = authDataService.getAgentAuthData(id);
+        List<TreeBO> hidList = authDataService.getHospitalAuthData(id);
+        if (aidList.size() == 0 && hidList.size() == 0) {
+            List<TreeBO> allList = authDataService.getAllAgentList();
+            return authDataService.treeBoToVo(allList);
+        }
+        if (hidList.size() > 0) {
+            TreeBO treeBO = new TreeBO();
+            treeBO.setId("AID0");
+            treeBO.setName("其他可选医院");
+            treeBO.setDisabled(true);
+            treeBO.setChildren(authDataService.toJsonString(hidList));
+            aidList.add(treeBO);
+        }
+        return authDataService.treeBoToVo(aidList);
+    }
+
 }
