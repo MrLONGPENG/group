086db36c39f364af5abd8beea7b39647819072b3
leolaure <leolaurel.e.l@gmail.com>
2018-12-05 22:56:53 +0800

    修改菜单树结构

--- lveqia-cloud-zuul/src/main/java/com/lveqia/cloud/zuul/service/impl/SysMenuServiceImpl.java
+++ lveqia-cloud-zuul/src/main/java/com/lveqia/cloud/zuul/service/impl/SysMenuServiceImpl.java
@@ -6,8 +6,7 @@ import com.lveqia.cloud.zuul.objeck.vo.MenuVo;
 import com.lveqia.cloud.zuul.service.SysMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author leolaurel
@@ -31,21 +30,52 @@ public class SysMenuServiceImpl implements SysMenuService {
 
     @Override
     public List<MenuVo> getMenusByUserId(Long id) {
+        if(id == 1) return getMenusByAdmin();
+        List<SysMenu> list = sysMenuMapper.getMenusByUserId(id);
+        Map<Integer, MenuVo> set = new LinkedHashMap<>();
+        Map<Integer, List<MenuVo>> map = new LinkedHashMap<>();
+        list.forEach(sysMenu -> addLinkMap(set, map, toMenuVo(sysMenu)));
+        return new ArrayList<>(set.values());
+    }
+
+    private void addLinkMap(Map<Integer, MenuVo> set, Map<Integer, List<MenuVo>> map, MenuVo menuVo) {
+        if(menuVo.getParentId() == null) {
+            set.put(menuVo.getId(), menuVo);
+        }else{
+            MenuVo parent  = toMenuVo(sysMenuMapper.findById(menuVo.getParentId()));
+            if(map.containsKey(menuVo.getParentId())){
+                map.get(menuVo.getParentId()).add(menuVo);
+            }else {
+                map.put(menuVo.getParentId(), new ArrayList<>(Collections.singletonList(menuVo)));
+            }
+            parent.setChildren(map.get(parent.getId()));
+            addLinkMap(set, map, parent);
+        }
+    }
+
+
+    private MenuVo toMenuVo(SysMenu sysMenu) {
+        MenuVo tree = new MenuVo();
+        tree.setId(sysMenu.getId());
+        tree.setPath(sysMenu.getPath());
+        tree.setName(sysMenu.getName());
+        tree.setIconCls(sysMenu.getIconCls());
+        tree.setParentId(sysMenu.getParentId());
+        tree.setComponent(sysMenu.getComponent());
+        tree.setMeta(sysMenu.getKeepAlive(), sysMenu.getRequireAuth());
+        return tree;
+    }
+
+    private List<MenuVo> getMenusByAdmin() {
         MenuVo tree;
         List<MenuVo> children;
         List<MenuVo> trees = new ArrayList<>();
         List<SysMenu> main = sysMenuMapper.getMainMenus(); // 获取一级菜单
-        List<SysMenu> list = id == 1 ? sysMenuMapper.getMenusByAdmin() : sysMenuMapper.getMenusByUserId(id);
+        List<SysMenu> list = sysMenuMapper.getMenusByAdmin();
         for (SysMenu sysMenu : main){
-            tree = new MenuVo(); // 当一级菜单拥有有权限的子菜单时候，放出一级菜单
             children = getChildren(list, sysMenu.getId());
             if (children != null && children.size() > 0) {
-                tree.setId(sysMenu.getId());
-                tree.setPath(sysMenu.getPath());
-                tree.setName(sysMenu.getName());
-                tree.setIconCls(sysMenu.getIconCls());
-                tree.setComponent(sysMenu.getComponent());
-                tree.setMeta(sysMenu.getKeepAlive(), sysMenu.getRequireAuth());
+                tree = toMenuVo(sysMenu); // 当一级菜单拥有有权限的子菜单时候，放出一级菜单
                 tree.setChildren(children);
                 trees.add(tree);
             }
@@ -59,13 +89,8 @@ public class SysMenuServiceImpl implements SysMenuService {
         List<MenuVo> trees = new ArrayList<>();
         for (SysMenu sysMenu : list) {
             if (parentId.equals(sysMenu.getParentId())){
-                tree = new MenuVo();
-                tree.setId(sysMenu.getId());
-                tree.setName(sysMenu.getName());
-                tree.setPath(sysMenu.getPath());
-                tree.setIconCls(sysMenu.getIconCls());
-                tree.setComponent(sysMenu.getComponent());
-                tree.setMeta(sysMenu.getKeepAlive(), sysMenu.getRequireAuth());
+                tree = toMenuVo(sysMenu);
+                tree.setChildren(getChildren(list, sysMenu.getId()));
                 trees.add(tree);
             }
         }
